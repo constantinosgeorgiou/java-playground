d8081c59c0d41734c37c02a2d620d8b436e5cd0f
Constantinos Georgiou <constantinos.georgiou1998@gmail.com>
2017-01-29 20:38:57 +0200

    Create step1.java

---
+++ step1.java
@@ -0,0 +1,11 @@
+/***
+ *
+ * @author Constantinos Georgiou
+ */
+public class Saluton {
+
+    public static void main(String[] arguments) {
+        String greeting = "Oh, hey there world!!!!";
+        System.out.println(greeting);
+    }
+}
